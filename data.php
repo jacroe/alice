a0dbac438b5b8b25a2a196ca8e5b15a48032457f
jacroe <jacob@jacroe.com>
2012-06-15 22:11:09 -0500

    Remove data.php and notif from git. Data is userbased and notif was extraneous.

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dWeather = array("currTemp"=>73, "currCond"=>"Clear", "hiTemp"=>98, "loTemp"=>68, "fcastTod"=>"Thunderstorm", "fcastTom"=>"Mostly Sunny");
-$dEmailCount = 0;
-$dLocation = array("city"=>"Hattiesburg","state"=>"MS","zip"=>39402,"lat"=>31.288632, "long"=>-89.351403, "tz"=>"America/Chicago","tz_short"=>"CDT");
-$dSunrise = "00:59";
-$dSunset = "13:01";
-$dIP = "208.149.64.52";
-$dUpdated = "10:00 pm";
-?>
