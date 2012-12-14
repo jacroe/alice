0db276018c76cb1e618457bdf2227d66ce2f1455
Jacob Roeland <jacob@jacroe.com>
2012-12-13 23:59:56 -0600

    Added xbmc stop button. Closes #30

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ if (alice_xbmc_check('playing'))
 	$subhead = <<<SHEAD
 <a class="btn btn-large" onclick='$.post("api.php", { control: "rewind" } );'><i class=icon-backward></i></a>
 <a class="btn btn-large btn-primary" onclick='$.post("api.php", { control: "pause" } );'><i class="icon-play icon-white"></i><i class="icon-pause icon-white"></i></a>
+<a class="btn btn-large" onclick='$.post("api.php", { control: "stop" } );'><i class=icon-stop></i></a>
 <a class="btn btn-large" onclick='$.post("api.php", { control: "forward" } );'><i class=icon-forward></i></a>
 <a class="btn btn-large" onclick='$.post("api.php", { control: "volume up" } );'><i class=icon-volume-up></i></a>
 <a class="btn btn-large" onclick='$.post("api.php", { control: "volume down" } );'><i class=icon-volume-down></i></a>
