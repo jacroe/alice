31f1f7e45f9bd2beff900c89fe9555a924c51965
Jacob Roeland <jacob@jacroe.com>
2013-03-22 03:24:14 -0500

    API begins returning data for some methods #45. Added grocery section "Deli"

--- api.php
+++ api.php
@@ -3,27 +3,51 @@ if (!$_POST['json']) die("This is Alice's API. She says hi.");
 require('alice.php');
 
 $json = json_decode(($_POST['json']));
-switch ($json->method) {
+switch ($json->method)
+{
 
 	// XBMC
 	case "XBMC.PlayFilm":
 		alice_xbmc_playFilm($json->params->id);
 		break;
 	case "XBMC.PlayEpisode":
-		alice_xbmc_playEpisode($json->params->id);
+		if($json->params->id)
+			alice_xbmc_playEpisode($json->params->id);
+		elseif($json->params->type)
+			if($json->params->type == "next")
+			{
+				$arrayNextEpisode = alice_xbmc_getFirstUnwatchedEpisode($json->params->showid);
+				alice_xbmc_playEpisode($arrayNextEpisode["id"]);
+			}
 		break;
 	case "XBMC.Control":
-		alice_xbmc_control($json->params->action);
+		$returnXBMC = alice_xbmc_control($json->params->action);
+		if($returnXBMC != -1)
+			echo alice_api_buildResponse("XBMC.Control", 1, $returnXBMC);
+		else
+			echo alice_api_buildResponse("XBMC.Control", 0, "Unrecognized control");
 		break;
 
 	// X10
 	case "X10.Do":
-		alice_x10($json->params->device, $json->params->action);
+		if(!$json->params->device || !$json->params->action)
+			echo alice_api_buildResponse("X10.Do", 0, "Invalid paramaters");
+		else
+		{
+			alice_x10($json->params->device, $json->params->action);
+			echo alice_api_buildResponse("X10.Do");
+		}
 		break;
 
 	// Timer
 	case "Timer.Set":
-		alice_timer_set($json->params->datetime, $json->params->message);
+		if(!$json->params->datetime || !$json->params->message)
+			echo alice_api_buildResponse("Timer.Set", 0, "Invalid paramaters");
+		else
+		{
+			$time = alice_timer_set($json->params->datetime, $json->params->message);
+			echo alice_api_buildResponse("Timer.Set", 1, $time);
+		}
 		break;
 
 	// Notifications
@@ -36,10 +60,22 @@ switch ($json->method) {
 
 	// Groceries
 	case "Grocery.Print":
-		alice_groceries_print();
+		if(alice_groceries_print())
+			echo alice_api_buildResponse("Grocery.Print");
+		else
+			echo alice_api_buildResponse("Grocery.Print", 0, "PDF was not placed in Dropbox. Check error.log for more info.");
 		break;
 
 	default:
-		echo "JSONERROR";
+		echo alice_api_buildResponse($json->method, 0, "Method not recognized.");
 		break;
+}
+function alice_api_buildResponse($method, $status = true, $response = null)
+{
+	if($status)
+		$json = array("method"=>$method, "status"=>"Ok", "response"=>$response);
+	else
+		$json = array("method"=>$method, "status"=>"Invalid", "error"=>$response);
+
+	return json_encode($json);
 }
--- modules/groceries.php
+++ modules/groceries.php
@@ -4,7 +4,7 @@ NAME:         Groceries Database
 ABOUT:        Stores and retrieves grocery information. Generates a shopping list.
 DEPENDENCIES: MySQL database (not the module);
 */
-function alice_groceries_get($arraySection = array("Health", "Cleaning supplies", "Dairy", "Grocery"))
+function alice_groceries_get($arraySection = array("Health", "Cleaning supplies", "Dairy", "Deli", "Grocery"))
 {
 	$db = new PDO('mysql:host='.MYSQL_SERVER.';dbname='.MYSQL_DB.';charset=utf8', MYSQL_USER, MYSQL_PASS);
 	$stmt = $db->prepare("SELECT name,aisle,price,needed,section FROM a_groceries WHERE (section LIKE :section) ORDER BY section,aisle DESC");
@@ -24,16 +24,22 @@ function alice_groceries_mark($item, $needed = true)
 	$stmt->execute(array(':name'=>"%$item%"));
 	return $stmt->rowCount();
 }
-function alice_groceries_print($arraySection = array("Health", "Cleaning supplies", "Dairy", "Grocery"))
+function alice_groceries_print($arraySection = array("Health", "Cleaning supplies", "Dairy", "Deli", "Grocery"))
 {
 	// This function only exists until the Events module is built. This shouldn't be done by this module
 	global $smarty;
 	$data = alice_groceries_get($arraySection);
 	$smarty->assign("data", $data);
-	
+
 	$body = $smarty->fetch("groceryList.tpl");
 	file_put_contents("/tmp/grocery.html", $body);
-	exec("xvfb-run -a wkhtmltopdf -s Letter /tmp/grocery.html /home/jacob/www/grocery.pdf");
-	#unlink("/tmp/grocery.html");
+	exec("xvfb-run -a wkhtmltopdf -s Letter /tmp/grocery.html ".DROPBOX."grocery.pdf");
+	unlink("/tmp/grocery.html");
 	exec("chmod 666 ".DROPBOX."grocery.pdf");
+	if(file_exists(DROPBOX."grocery.pdf")) return 1;
+	else
+	{
+		alice_error_add("Grocery Module", "Grocery printing error - File not moved to Dropbox");
+		return 0;
+	}
 }
