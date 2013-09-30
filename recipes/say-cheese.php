1458fe863766ffc25d916a376c1e4d7f9c892ca0
Jacob Roeland <jacob@jacroe.com>
2013-09-29 20:36:27 -0500

    Updated say-cheese to use the fswebcam instead of ffmpeg

--- recipes/say-cheese.php
+++ recipes/say-cheese.php
@@ -2,7 +2,7 @@
 /*
 NAME:         Say cheese!
 ABOUT:        Takes a photo from the webcam and stores it in Dropbox. Note: This recipe as is will NOT work on Windows. It has only been tested to work on Ubuntu 11.10; it may work on Mac.
-DEPENDENCIES: MySQL module; ffmpeg; ImageMagick;
+DEPENDENCIES: MySQL module; fswebcam; ImageMagick;
 INSTALL:      None;
 CONFIG:       Change the location of your webcam and where you would like the image to be stored;
 */
@@ -11,7 +11,7 @@ if (!file_exists(DROPBOX."webcam/".date('Y/m/d/'))) mkdir(DROPBOX."webcam/".date
 if (!(date('Hi') % 2))
 {
 	$time = date("M j, Y g:i a");
-	exec("ffmpeg -f video4linux2 -i /dev/video0 -vframes 1 ".DROPBOX."webcam/$imgLoc.jpg && convert ".DROPBOX."webcam/$imgLoc.jpg -gravity southeast -stroke none -fill white -annotate 0 '$time' ".DROPBOX."webcam/latest.jpg");
+	exec("fswebcam -r 640x480 --jpeg 85 -D 1 --no-banner ".DROPBOX."webcam/$imgLoc.jpg && convert ".DROPBOX."webcam/$imgLoc.jpg -gravity southeast -stroke none -fill white -annotate 0 '$time' ".DROPBOX."webcam/latest.jpg");
 	alice_mysql_putImage("webcam_latest", file_get_contents(DROPBOX."webcam/latest.jpg"), "image/jpg");
 }
 ?>
